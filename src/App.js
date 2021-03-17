73fe25e9be90eeef1ea08077fe1c7b71a65a98bd
Mike MacCana <mike.maccana@gmail.com>
2021-03-17 12:42:18 +0000

    Lots of small cleanups

--- src/App.js
+++ src/App.js
@@ -5,38 +5,37 @@ const log = console.log.bind(console);
 
 export default function App() {
   const round = Math.round;
+  const HEIGHT = 22
 
   let [topOffset, setTopOffset] = useState(0);
   let [rightOffset, setRightOffset] = useState(0);
   let [bottomOffset, setBottomOffset] = useState(0);
   let [leftOffset, setLeftOffset] = useState(0);
-  let [height, setHeight] = useState(0);
   let [width, setWidth] = useState(0);
 
   log(`HELLO WORLD`);
 
   useEffect(() => {
+    const article = document.querySelector("article");
+    const articleRect = article.getBoundingClientRect();
+    log('articleRect', articleRect)
+
+
     const highlighted = document.querySelector(".highlighted");
-
-    const documentOffsetRect = highlighted.getBoundingClientRect();
-
-    log(`documentOffsetRect`, documentOffsetRect);
-
     const rects = highlighted.getClientRects();
     const lastRect = rects[rects.length - 1];
     log(`lastRect`, lastRect);
 
-    setTopOffset(round(lastRect.bottom - 8));
-    setLeftOffset(round(lastRect.left - 8));
+    setTopOffset(round(lastRect.bottom - articleRect.top));
+    setLeftOffset(round(lastRect.left - articleRect.left));
 
-    setHeight(round(lastRect.height));
+    
     setWidth(round(lastRect.width));
   });
 
   return (
     <div className="App">
-      <div
-        className="text"
+      <article
         style={{
           maxWidth: "300px",
           position: "relative"
@@ -45,7 +44,29 @@ export default function App() {
         <span>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud
+          minim veniam, quis nostrud reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident,sunt in culpa
+          qui officia deserunt mollit anim id est laborum. in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident,sunt in culpa
+          qui officia deserunt mollit anim id est laborum.in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident,sunt in culpa
+          qui officia deserunt mollit anim id est laborum.in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident,sunt in culpa
+          qui officia deserunt mollit anim id est laborum.in voluptate velit esse cillum dolore eu fugiat nulla
+
+          pariatur. Excepteur sint occaecat cupidatat non proident,sunt in culpa
+          qui officia deserunt mollit anim id est laborum.in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident,sunt in culpa
+          qui officia deserunt mollit anim id est laborum.in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident,sunt in culpa
+          qui officia deserunt mollit anim id est laborum.in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident,sunt in culpa
+          qui officia deserunt mollit anim id est laborum.in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident,sunt in culpa
+          qui officia deserunt mollit anim id est laborum.in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident,sunt in culpa
+          qui officia deserunt mollit anim id est laborum.in voluptate velit esse cillum dolore eu fugiat nulla
+          
           <span
             className="highlighted"
             style={{
@@ -55,7 +76,38 @@ export default function App() {
             exercitation ullamco laboris nisi ut aliquip ex ea commodo
             consequat. Duis aute irure dolor in
           </span>
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident,sunt in culpa
+          qui officia deserunt mollit anim id est laborum.in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident,sunt in culpa
+          qui officia deserunt mollit anim id est laborum.in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident,sunt in culpa
+          qui officia deserunt mollit anim id est laborum.in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident,sunt in culpa
+          qui officia deserunt mollit anim id est laborum.in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident,sunt in culpa
+          qui officia deserunt mollit anim id est laborum.in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident,sunt in culpa
+          qui officia deserunt mollit anim id est laborum.in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident,sunt in culpa
+          qui officia deserunt mollit anim id est laborum.in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident,sunt in culpa
+          qui officia deserunt mollit anim id est laborum.in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident,sunt in culpa
+          qui officia deserunt mollit anim id est laborum.in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident,sunt in culpa
+          qui officia deserunt mollit anim id est laborum.in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident,sunt in culpa
+          qui officia deserunt mollit anim id est laborum.in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident,sunt in culpa
+          qui officia deserunt mollit anim id est laborum.in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident,sunt in culpa
+          qui officia deserunt mollit anim id est laborum.in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident,sunt in culpa
+          qui officia deserunt mollit anim id est laborum.in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident,sunt in culpa
+          qui officia deserunt mollit anim id est laborum.in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident,sunt in culpa
+          qui officia deserunt mollit anim id est laborum.in voluptate velit esse cillum dolore eu fugiat nulla
           pariatur. Excepteur sint occaecat cupidatat non proident,sunt in culpa
           qui officia deserunt mollit anim id est laborum.
         </span>
@@ -66,21 +118,20 @@ export default function App() {
             position: "absolute",
             display: "block",
             zIndex: 10,
+            opacity: 0.5,
 
             top: `${topOffset}px`,
-            right: `${rightOffset}px`,
-            bottom: `${bottomOffset}px`,
             left: `${leftOffset}px`,
 
-            height: `${height}px`,
+            height: `${HEIGHT}px`,
             width: `${width}px`,
 
-            backgroundColor: "red"
+            backgroundColor: "rebeccapurple"
           }}
         >
           label
         </div>
-      </div>
+      </article>
     </div>
   );
 }
